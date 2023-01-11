5673e10d4aa55127e2736dd272f4e3a7a02bdd52
varsha <varshakakumanu@gmail.com>
2023-01-11 10:31:37 +0530

    adding router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     welcome
+     <h5>CRUD opreations</h5>
+
+     <BrowserRouter>
+     <nav>
+      <ul>
+        <li>
+          <Link to ="/">List users</Link>
+        </li>
+        <li>
+          <Link to ="users/create">create users</Link>
+        </li>
+      </ul>
+     </nav>
+     </BrowserRouter>
     </div>
   );
 }
